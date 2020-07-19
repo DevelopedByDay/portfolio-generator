2537525f89b7396997be575c7153799ed4130510
BradD <bradleyeday@gmail.com>
2020-07-19 18:33:48 -0500

    index.html generated

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.lenght);
 const [name, github] = profileDataArgs;
 
@@ -19,5 +21,10 @@ const generatePage = (userName, githubName) => {
 </html>
 `;
 };
-console.log(name, github);
-console.log(generatePage(name, github));
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output')
+});
