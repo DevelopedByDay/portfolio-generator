d24d24e6aaf2dec56e2fb1741579e1ba56e096aa
BradD <bradleyeday@gmail.com>
2020-07-21 13:37:34 -0500

    inquirer framework added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(portfolioData);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-    console.log('Portfolio complete! Check out index.html to see the output')
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output')
+// });
+
