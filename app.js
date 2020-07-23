e72eb953f0512c79316a04d6a0d1646c9b91df14
BradD <bradleyeday@gmail.com>
2020-07-23 09:13:33 -0500

    generate HTML structure

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(portfolioData);
+
 
 const promptUser = () => {
     return inquirer
@@ -134,14 +134,15 @@ const promptProject = portfolioData => {
 };
     
 
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output')
-// });
 
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('index.html', generatePage(portfolioData), err => {
+        if (err) throw err;
+
+        console.log('Portfolio complete! Check out index.html to see the output')
+    });
 });
